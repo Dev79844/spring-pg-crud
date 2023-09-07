ceece7baaa6d043824673bddef9910a998915c79
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-09-07 20:28:55 +0530

    crud operations

---
+++ src/main/java/com/dev/crudpg/CrudPgApplication.java
@@ -0,0 +1,13 @@
+package com.dev.crudpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/crudpg/CrudPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.crudpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
